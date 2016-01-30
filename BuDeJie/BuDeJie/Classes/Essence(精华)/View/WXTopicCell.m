77ec557692e4cfbe5320ec9a2163a5659de569b7
李伟雄 <liwx@liwxdemacbook-pro.local>
2016-01-30 18:56:12 +0800

    1.处理底部按钮文字显示

--- BuDeJie/BuDeJie/Classes/Essence(精华)/View/WXTopicCell.m
+++ BuDeJie/BuDeJie/Classes/Essence(精华)/View/WXTopicCell.m
@@ -33,15 +33,35 @@
 {
     _topicItem = topicItem;
     
+    
     [self.profileImageView sd_setImageWithURL:[NSURL URLWithString:topicItem.profile_image]];
     self.nameLabel.text = topicItem.name;
     self.createdAtLabel.text = topicItem.created_at;
     self.text_label.text = topicItem.text;
-    [self.dingButton setTitle:[NSString stringWithFormat:@"%ld", topicItem.ding] forState:UIControlStateNormal];
-    [self.caiButton setTitle:[NSString stringWithFormat:@"%ld", topicItem.cai] forState:UIControlStateNormal];
-    [self.repostButton setTitle:[NSString stringWithFormat:@"%ld", topicItem.repost] forState:UIControlStateNormal];
-    [self.commentButton setTitle:[NSString stringWithFormat:@"%ld", topicItem.comment] forState:UIControlStateNormal];
+    // ------------------------------------------------------------------------
+    // 底部按钮文字显示处理
+    [self setButton:self.dingButton number:topicItem.ding placeholder:@"顶"];
+    [self setButton:self.caiButton number:topicItem.cai placeholder:@"踩"];
+    [self setButton:self.repostButton number:topicItem.repost placeholder:@"分享"];
+    [self setButton:self.commentButton number:topicItem.comment placeholder:@"评论"];
+    
     self.topCmtContentLabel.text = @"最热评论测试数据最热评论测试数据最热评论测试数据最热评论测试数据";
+}
+
+// ------------------------------------------------------------------------
+// 底部按钮文字显示处理
+- (void)setButton:(UIButton *)button number:(NSInteger)number placeholder:(NSString *)placeholder
+{
+    NSString *title = nil;
+    if (number >= 10000) {
+        title = [NSString stringWithFormat:@"%.1f万", number / 10000.0];
+    }else if (number > 0) {
+        title = [NSString stringWithFormat:@"%ld", number];
+    }else {
+        title = placeholder;
+    }
+    
+    [button setTitle:title forState:UIControlStateNormal];
 }
 
 @end
